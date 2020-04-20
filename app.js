93bf9e73bdb8d90d70f6e4c3abe74bd05a31f58b
RodriguezRuizI <33327123+RodriguezRuizI@users.noreply.github.com>
2020-04-20 17:44:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var	bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var path = require('path');
 
